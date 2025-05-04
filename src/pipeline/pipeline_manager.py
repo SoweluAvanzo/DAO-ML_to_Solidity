e19e8882a991ff01f6e78d0a9604bfee02034d73
lonevetad <marco.1995.ottina@gmail.com>
2025-05-04 13:54:46 +0200

    WIP: refactoring dependency_outputs to dependency_inputs
    Signed-off-by: lonevetad <marco.1995.ottina@gmail.com>

--- src/pipeline/pipeline_manager.py
+++ src/pipeline/pipeline_manager.py
@@ -10,14 +10,16 @@ class NodeRunStatus(Enum):
     RUNNING = 2
     DONE = 3
 
-def newEmptyDependencyOutputData(done=False, data=None) -> list[bool, any]:
+def newEmptyDependencyOutputData(done=False, data=None): # -> list[bool, any]:
     return [done, data]
-class PipelineNode:
-    def __init__(self, item: pi.PipelineItem):
+class PipelineNode(pi.PipelineItem):
+    def __init__(self, pipelineManager, item: pi.PipelineItem):
+        super().__init__("", None)
+        self.pipelineManager=pipelineManager
         self.item = item
         self.status_run = NodeRunStatus.NEVER_RUN
         self.dependants:dict[str, pi.PipelineItem] = {}
-        self.dependency_outputs:dict[str, list[bool, any]] = {}
+        self.dependency_outputs:dict[str, list[bool, any]] = {} # TODO OOOOOOOOOOO RINOMINARE IN "_input" e gestirlo
     def getItem(self):
         return self.item
     def getDependencies(self):
@@ -27,8 +29,9 @@ class PipelineNode:
     def addDependant(self, newDependant: pi.PipelineItem):
         key = newDependant.key
         self.dependants[key] = newDependant
-        self.dependency_outputs[key] = newEmptyDependencyOutputData()
-    def getInputForRun(self)->dict[str,any]:
+        # self.dependency_outputs[key] = newEmptyDependencyOutputData()
+        self.pipelineManager. ... nodes[key].dependency_inputs[self.item.key] = newEmptyDependencyOutputData()
+    def getInputForRun(self): # ->dict[str,any]:
         return { ite[0]: ite[1][1] for ite in self.dependency_outputs.items()}
     def canRun(self):
         if self.status_run != NodeRunStatus.NEVER_RUN:
@@ -50,6 +53,18 @@ class PipelineNode:
         else:
             output_data = newEmptyDependencyOutputData(True, input_value)
             self.dependency_outputs[key] = output_data
+    def repr_inner(self):
+        return """
+            "item": {{
+                    {0}
+            }},
+            "status": "{1}",
+            "dependants": {2},
+            "dependency_outputs": {3}    
+        """.format(repr(self.item), \
+                    self.status_run, \
+                    str(list(self.dependants.keys())), \
+                    repr(self.dependency_outputs))
 
 class PipelineManager:
     def __init__(self):
@@ -63,12 +78,19 @@ class PipelineManager:
             if item.key == key:
                 return item
         return None
+    
+    def printItems(self, itemsMap):
+        # PRINT EVERYTHING
+        print("\n\nPRINT all")
+        for key, item in itemsMap.items():
+            print(f"{key} ) -> {repr(item)}") # TODO: DEPENDENCIES ED ALTROOOOOOOOOOOOOOOOOOOOo
+        print("\n\n")
 
     def runPipeline(self) -> any:
         print("START RUN PIPELINE")
         #setup the data structures
         items_as_list = list(self.items.values())
-        nodes = { item.key: PipelineNode(item) for item in items_as_list }
+        nodes = { item.key: PipelineNode(self, item) for item in items_as_list }
         roots = []
         # setup dependendants
         for item in items_as_list:
@@ -80,17 +102,18 @@ class PipelineManager:
                         n = nodes[d]
                         n.addDependant(item)
             else:
-                roots.append(item)
                 n = nodes[item.key]
+                roots.append(n)
                 n.status_run = NodeRunStatus.QUEUED
         job_queues = deque(roots) # a.k.a. "frontier" ; single thread -> only one item at a time can run
         del roots
+        self.printItems(nodes)
         #RUN
-        print("actual start of running")
+        print(f"actual start of running: starting with {len(job_queues)} jobs in queue")
         while len(job_queues) > 0:
             job:PipelineNode = job_queues.popleft()
             job.status_run = NodeRunStatus.RUNNING
-            print(f"running job {job.item.key}")
+            print(f"running job {job.item.key} with dependencies {job.item.dependencies} and dependants {job.getDependants()}")
             input = job.getInputForRun()
             try:
                 output = job.item.run(input)
---
+++ test_pi.py
@@ -0,0 +1,99 @@
+import src.pipeline.pipeline_manager as pmp
+import src.pipeline.pipeline_item as pip
+
+
+class A:
+    def __init__(self, text="", num=0):
+        self.text = text
+        self.num=num
+    def getNum(self):
+        return self.num
+    def __repr__(self):
+        return """{{"text": "{0}", "num": {1} }}""".format(self.text, str(self.getNum()))
+    
+class B(A):
+    def __init__(self, text="", num=0):
+        super().__init__(text, num)
+    def getNum(self):
+        return self.num * 1000
+
+
+
+class PIPrinter(pip.PipelineItem):
+    def __init__(self, key, dependencies=None, text="", fromInput=False):
+        super().__init__(key, dependencies)
+        self.text=text
+        self.fromInput = fromInput
+    def run(self, input):
+        t = input[self.dependencies[0]] if  self.fromInput else self.text
+        print(f"printing: {t}")
+        return input
+    def repr_inner(self):
+        return """ "text":"{0}" """.format(self.text)
+
+class PIInt(pip.PipelineItem):
+    def __init__(self, key, dependencies=None, val=0):
+        super().__init__(key, dependencies)
+        self.val=val
+    def run(self, input):
+        return self.val
+    def repr_inner(self):
+        return """ "val": {0}""".format(str(self.val))
+
+
+class PIMath(pip.PipelineItem):
+    def __init__(self, key, dependencies=None, scalar=0, isAddition=True):
+        super().__init__(key, dependencies)
+        self.scalar=scalar
+        self.isAddition=isAddition
+    def run(self, input):
+        val = input[self.dependencies[0]] # input['value'] if 'value' in input else 0
+        print(f"{ 'adding' if self.isAddition else 'multiplying'} {val} with {self.scalar}")
+        return self.scalar + val if self.isAddition else self.scalar * val
+    #def __repr__(self):
+    def repr_inner(self):
+        #return """ "val": {0}""".format(str(self.val))
+        return """ "isAddition": {0}, "scalar": {1}""".format( 'true' if self.isAddition else 'false', self.scalar)
+
+
+from enum import Enum
+
+class TD:
+    def __init__(self, k,d,altro=None):
+        print(f"k:{k}, d: {d}")
+        self.k=k
+        self.d=d
+        self.altro=altro
+    def __repr__(self):
+        return "{{ k:{0}, d: {1}, altro: {2}}}".format(self.k, self.d, str(self.altro))
+
+class TestData(Enum):
+        START = TD("START", "let's begin")
+        C1 = TD("C1", 10)
+        V1d= TD("V1",5)
+        # V2d = TD("V2",7)
+
+if __name__ == "__main__":
+    # a = A("aaaa", 5)
+    # b = B( "BBBB", 3)
+    # print(repr(a))
+    # print(repr(b))
+
+    print("AAAAAAAAAAAAAA")
+
+    pm = pmp.PipelineManager()
+    
+    pi = PIPrinter(TestData.START.value.k, [], TestData.START.value.d)
+    pm.addItem(pi)
+
+    pi = PIInt(TestData.C1.value.k, [TestData.START.value.k], TestData.C1.value.d)
+    pm.addItem(pi)
+    
+    pi = PIPrinter("echo", [TestData.C1.value.k], None, True)
+    pm.addItem(pi)
+
+    pm.runPipeline()
+
+
+    print("BBBBBBBBBBBBBB")
+    
