a91be775a34346bf0995acf8c4e4890434fa44ba
lonevetad <marco.1995.ottina@gmail.com>
2025-05-06 06:44:58 +0200

    IMproved test, added possibility to append new dependants to PipelineItems and remove items to the PipelineManager
    Signed-off-by: lonevetad <marco.1995.ottina@gmail.com>

--- src/pipeline/pipeline_item.py
+++ src/pipeline/pipeline_item.py
@@ -2,12 +2,15 @@
 class PipelineItem:
     def __init__(self, key:str, dependencies=None):
         self.key = key
-        self.dependencies = dependencies
+        self.dependencies:list[str] = dependencies
     
     def run(self, input): # input:dict[str, any]) -> any:
         print(f"ERROR: PipelineItem has not implemented the run method. Key:")
         print(self.key)
         pass
+
+    def addDependency(self, newDependency):
+        self.dependencies.append(newDependency)
 
     def repr_inner(self):
         return ""
