7f1fdb2424d89d251fe30b9d8e5ad6da15a60d52
lonevetad <marco.1995.ottina@gmail.com>
2025-06-07 23:09:28 +0200

    WIP: Started creation of XML validator
    Signed-off-by: lonevetad <marco.1995.ottina@gmail.com>

--- src/validators/base_validator.py
+++ src/validators/base_validator.py
@@ -1,12 +1,12 @@
 import src.pipeline.pipeline_item as pi
 
 
-class ValidatorBase(pi.PipelineItem):
+class BaseValidator(pi.PipelineItem):
     def __init__(self, pipeline_item_data: pi.PIData ):
         super().__init__(pipeline_item_data)
 
     def validate(self, input:dict) -> bool:
         return False
     
-    def run(self, inputs):
-        return self.validate(inputs)
+    def run(self, inputs: dict):
+        return self.validate(inputs[self.get_dependencies()[0]])
