7201b324d00a50408d665df7a262d9575e7bd7ae
lonevetad <marco.1995.ottina@gmail.com>
2025-06-29 22:20:39 +0200

    recovered some of the files and changes from the branch about jinja template translation
    Signed-off-by: lonevetad <marco.1995.ottina@gmail.com>

--- src/postprocessing/model_conversion/solidity/solidity_translator_optimized.py
+++ src/postprocessing/model_conversion/solidity/solidity_translator_optimized.py
@@ -6,3 +6,8 @@ class TranslatorOptimized(sol_transl_general.TranslatorGeneral):
     
     def __init__(self, pipeline_item_data: pi.PIData):
         super().__init__(pipeline_item_data)
+
+    def translate(self, additional_data=None):
+
+        # See " optimized_translator.py # OptimizedSolidityTranslator"
+        return None
