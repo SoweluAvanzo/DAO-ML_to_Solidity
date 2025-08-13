66ae8a5eedcbcfc394772dc4917c9537dd35dac8
lonevetad <marco.1995.ottina@gmail.com>
2025-08-14 00:44:06 +0200

    WIP: starting preparation of Committee and related voting protocol conversion (and later the compilation)
    Signed-off-by: lonevetad <marco.1995.ottina@gmail.com>

--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -1,3 +1,4 @@
+import regex as re
 
 def is_string_or_list(t):
     if isinstance(t, list):
@@ -6,3 +7,13 @@ def is_string_or_list(t):
         return True
     else:
         return None
+    
+
+def to_camel_case(s:str):
+    parts = re.split(r'-|_', s)
+    if len(parts) <= 1:
+        return s
+    return parts[0] + "".join(\
+        f"{str(p[0]).upper()}{p[1:]}" if len(p) > 1 else ''
+        for p in parts[1:]
+    )
