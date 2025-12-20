32de2800f72619d48578280a2e853c4511adc699
Marco Ottina <marco.1995.ottina@gmail.com>
2025-12-20 23:05:10 +0100

    WIP: started JSONValidator (to validate input)
    Signed-off-by: Marco Ottina <marco.1995.ottina@gmail.com>

---
+++ src/parsers/json/JSONValidator.py
@@ -0,0 +1,6 @@
+import src.pipeline.pipeline_item as pi
+
+
+class JSONValidator(pi.PipelineItem):
+
+    # TODO: deve ricevere un oggetto (JSON/dict) e verificare che rispetti il modello denro il model
